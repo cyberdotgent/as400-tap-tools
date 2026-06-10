14335bff39608fd8f78d2aef9b2a74ff0efe7550
Yvan Janssens <yvanj@cyber.gent>
2026-06-10 09:46:34 +0200

    Add application icon support for all platforms

--- src/App.cpp
+++ src/App.cpp
@@ -3,7 +3,11 @@
 #include "MainFrame.h"
 
 #include <wx/cmdline.h>
+#include <wx/filename.h>
+#include <wx/image.h>
+#include <wx/icon.h>
 #include <wx/string.h>
+#include <wx/stdpaths.h>
 
 wxIMPLEMENT_APP(TapeToolsApp);
 
@@ -18,6 +22,17 @@ std::filesystem::path PathFromWxString(const wxString& value)
 #endif
 }
 
+wxString AppIconPath()
+{
+    const auto& standard_paths = wxStandardPaths::Get();
+#if defined(__APPLE__)
+    return wxFileName(standard_paths.GetResourcesDir(), "as400-tap-tools.png").GetFullPath();
+#else
+    wxFileName executable(standard_paths.GetExecutablePath());
+    return wxFileName(executable.GetPath(), "assets/as400-tap-tools.png").GetFullPath();
+#endif
+}
+
 } // namespace
 
 bool TapeToolsApp::OnInit()
@@ -26,7 +41,16 @@ bool TapeToolsApp::OnInit()
         return false;
     }
 
+    wxInitAllImageHandlers();
+
     auto* frame = new MainFrame(wxString::FromUTF8("AS400 Tape Tools"));
+    const auto icon_path = AppIconPath();
+    if (wxFileName::FileExists(icon_path)) {
+        wxIcon icon(icon_path, wxBITMAP_TYPE_PNG);
+        if (icon.IsOk()) {
+            frame->SetIcon(icon);
+        }
+    }
     frame->Show(true);
     if (startup_tape_path_) {
         frame->LoadTapeFile(*startup_tape_path_);
